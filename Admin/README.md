7c036b4d39050acf02f68cebf9b521c26433f674
Sebastian Ganoza <sebastian.ganoza73@gmail.com>
2024-01-27 15:58:23 -0500

    Proyecto Final

---
+++ Admin/README.md
@@ -0,0 +1,2 @@
+Buenas profesor emmm la pagina funciona todo okey, el crud solo funciona con mi correo porque soy el admin, entonces no se si enviarle mi correo XD.
+Pero bueno si desea mi correo me lo pide por discord y se lo pasaré, espero puedo leerlo.
---
+++ Admin/actions/get-graph-revenue.ts
@@ -0,0 +1,60 @@
+import prismadb from "@/lib/prismadb";
+
+interface GraphData {
+  name: string;
+  total: number;
+}
+
+export const getGraphRevenue = async (storeId: string): Promise<GraphData[]> => {
+  const paidOrders = await prismadb.order.findMany({
+    where: {
+      storeId,
+      isPaid: true,
+    },
+    include: {
+      orderItems: {
+        include: {
+          product: true,
+        },
+      },
+    },
+  });
+
+  const monthlyRevenue: { [key: number]: number } = {};
+
+ 
+  for (const order of paidOrders) {
+    const month = order.createdAt.getMonth(); 
+    let revenueForOrder = 0;
+
+    for (const item of order.orderItems) {
+      revenueForOrder += item.product.price.toNumber();
+    }
+
+   
+    monthlyRevenue[month] = (monthlyRevenue[month] || 0) + revenueForOrder;
+  }
+
+ 
+  const graphData: GraphData[] = [
+    { name: "Enero", total: 0 },
+    { name: "Febrero", total: 0 },
+    { name: "Marzo", total: 0 },
+    { name: "Abril", total: 0 },
+    { name: "Mayo", total: 0 },
+    { name: "Junio", total: 0 },
+    { name: "Julio", total: 0 },
+    { name: "Agosto", total: 0 },
+    { name: "Septiembre", total: 0 },
+    { name: "Octubre", total: 0 },
+    { name: "Noviembre", total: 0 },
+    { name: "Diciembre", total: 0 },
+  ];
+
+  
+  for (const month in monthlyRevenue) {
+    graphData[parseInt(month)].total = monthlyRevenue[parseInt(month)];
+  }
+
+  return graphData;
+};
